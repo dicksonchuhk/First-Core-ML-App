7feddceb8d8c368a3f28ecc6b7ac25826271800c
Dickson Chu <dickson.yw.chu@gtomato.com>
2019-07-11 23:24:55 +0800

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Dickson Chu on 11/7/2019.
+//  Copyright © 2019 Dickson Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
